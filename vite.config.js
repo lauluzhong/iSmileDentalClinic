fa4ac76f74324f8398c1ab5b794a28b4c0a8f4be
Lu Zhong <luzhong@ismile.com.my>
2026-03-15 01:34:47 +0000

    Revert "fix: add prerender-spa-plugin for SEO meta tags"
    This reverts commit 92381df692f0993605a8afe33d8ae5016515c6cc.

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import PrerenderSPAPlugin from 'prerender-spa-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    new PrerenderSPAPlugin({
-      routes: [
-        '/',
-        '/about',
-        '/services',
-        '/blog',
-        '/contact',
-        '/services/children/myobrace',
-        '/services/children',
-        '/services/protect',
-        '/services/straighten',
-        '/services/replace',
-        '/services/其它'
-      ],
-      renderTarget: '#app',
-      postProcess: (html) => {
-        // Remove any script tags that might break prerendering
-        return html
-      }
-    })
-  ],
+  plugins: [react()],
 })
