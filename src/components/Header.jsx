84711c077b13bd9388f57f89fa86c8bad233f37f
iSmile Engineer (Edith) <ismiledental76@gmail.com>
2026-07-28 07:03:14 +0200

    Nav: remove (Invisalign) bracket from Clear Aligners dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -67,7 +67,7 @@ const Header = () => {
             dropdown: [
                 { label: "Featured Services" },
                 { name: 'Myofunctional Orthodontics', path: '/services/children/myofunctional' },
-                { name: 'Clear Aligners (Invisalign)', path: '/services/straighten/clear-aligners' },
+                { name: 'Clear Aligners', path: '/services/straighten/clear-aligners' },
                 { name: 'Dental Implants', path: '/services/replace/dental-implants' },
                 { name: 'Crowns & Bridges', path: '/services/protect/root-canal' },
                 { name: 'Cosmetic Dentistry', path: '/services/enhance/cosmetic-dentistry' },
